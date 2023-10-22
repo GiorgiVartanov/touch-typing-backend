d1e6520f79492a2b9cc32878c28a951b83593fbe
PhYell <94902895+PhYell@users.noreply.github.com>
2023-10-23 01:04:31 +0400

    added cors

--- src/index.ts
+++ src/index.ts
@@ -15,7 +15,7 @@ const port = process.env.PORT || 5000
 
 connectDB()
 
-app.use(cors())
+app.use(cors({ origin: process.env.CLIENT_URL }))
 
 app.use(express.json())
 app.use(express.urlencoded({ extended: false }))
