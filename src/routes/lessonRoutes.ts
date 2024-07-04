d6de225a25573a8bd0fd5dbbd21738062cf9026c
GiorgiVartanov <minisk50@gmail.com>
2024-07-04 22:12:46 +0400

    added route for assessment complition

--- src/routes/lessonRoutes.ts
+++ src/routes/lessonRoutes.ts
@@ -1,10 +1,12 @@
 import express, { Router } from "express"
+import protect from "../middleware/authMiddleware"
 
 const router: Router = express.Router()
 
-import { getAssessment, getExercise } from "../controllers/lessonController"
+import { getAssessment, getExercise, completeAssessment } from "../controllers/lessonController"
 
 router.get("/exercise", getExercise)
 router.get("/assessment", getAssessment)
+router.post("/assessment", protect, completeAssessment)
 
 export default router
